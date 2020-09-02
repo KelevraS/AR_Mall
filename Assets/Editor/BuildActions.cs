d9b5cd3eef1091e3572c5570e6c83bc9fc62650f
Maksim <maksim.melnik.konstantinovich@gmail.com>
2020-09-02 15:50:02 +0300

    Added iOS Development to Build Pipeline

--- Assets/Editor/BuildActions.cs
+++ Assets/Editor/BuildActions.cs
@@ -12,8 +12,11 @@ namespace Game
     {
         static string GAME_NAME = "my_game";
         static string VERSION = Application.version;
-        public static string APP_FOLDER = Directory.GetCurrentDirectory ();
-        public static string ANDROID_DEVELOPMENT_FILE = string.Format ("{0}/Builds/Android/{1}.{2}.development.apk", APP_FOLDER, GAME_NAME, VERSION);
+        static string APP_FOLDER = Directory.GetCurrentDirectory ();
+        static string ANDROID_DEVELOPMENT_FILE = string.Format ("{0}/Builds/Android/{1}.{2}.development.apk", APP_FOLDER, GAME_NAME, VERSION);
+        static string IOS_FOLDER = string.Format("{0}/Builds/iOS/", APP_FOLDER);
+        static string IOS_DEVELOPMENT_FOLDER = string.Format("{0}/Builds/iOS/build/development", APP_FOLDER);
+        
 
         
         static string[] GetScenes () {
@@ -24,6 +27,17 @@ namespace Game
                 }
             }
             return scenes.ToArray ();
+        }
+
+        static void iOSDevelopment()
+        {
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.iOS, ScriptingImplementation.IL2CPP);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, null);
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.iOS);
+            BuildReport report = BuildPipeline.BuildPlayer(GetScenes(), IOS_DEVELOPMENT_FOLDER, BuildTarget.iOS,
+                BuildOptions.None);
+            int code = report.summary.result == BuildResult.Succeeded ? 0 : 1;
+            EditorApplication.Exit(code);
         }
         
         static void AndroidDevelopment () {
--- BuildPipeline.sh
+++ BuildPipeline.sh
@@ -10,6 +10,7 @@ BOT_TOKEN='1319154477:AAE0WQ6rRCI5x6_Iev8fanCFPJP2Ypjk7Hs'
 CHAT_ID='-419319501'
 
 LOGS_PATH=$PROJECT_PATH'/Logs'
+IOS_PATH=$PROJECT_PATH'/iOS'
 
 UNITY='D:/Program Files/Unity/2019.4.9f1/Editor/Unity.exe'
 
@@ -29,6 +30,39 @@ git checkout $BRANCH >> "$LOGS_PATH/git.log" 2>&1
 git pull >> "$LOGS_PATH/git.log" 2>&1
 SendTelegramMessage "Successfully updated repo $BRANCH..."
 }
+
+function iOSDevelopment {
+echo '' 
+echo '|||||||||||||||||||||||||||||||' 
+echo '|                             |' 
+echo '|     iOS development     |' 
+echo '|                             |' 
+echo '|||||||||||||||||||||||||||||||' 
+echo ''
+echo ''
+if [ -d $"IOS_PATH" ]; then
+echo ''
+echo 'Clean iOS directory'
+echo ''
+rm -r $IOS_PATH
+fi
+echo ''
+echo 'Build Unity...'
+echo ''
+'D:/Program Files/Unity/2019.4.9f1/Editor/Unity.exe' -batchmode -quit -projectPath 'D:\Unity\Projects\AR_Mall' -executeMethod Game.BuildActions.iOSDevelopment -buildTarget ios -logFile "$LOGS_PATH/ios_development.log"
+if [ $? -ne 0 ]; then
+echo ''
+echo 'Operation failed!'
+SendTelegramMessage 'Operation failed!'
+echo '' 
+exit 1
+fi
+echo ''
+echo 'Build XCode...'
+SendTelegramMessage "iOS build completed"
+echo ''
+}
+
 
 function AndroidDevelopment {
 echo '' 
@@ -55,6 +89,16 @@ SendTelegramMessage 'Build completed'
 echo '' 
 }
 
+function ShowElapsedTime
+{
+echo ''
+end=$(date +"%s")
+elapsed=$(($end-$START))
+echo "$(($elapsed / 60)) minutes $(($elapsed % 60)) seconds"
+SendTelegramMessage "$(($elapsed / 60)) minutes $(($elapsed % 60)) seconds"
+echo ''
+}
+
 echo '' 
 echo '' 
 echo '' 
@@ -68,6 +112,7 @@ echo ''
 echo ''
 echo "0 - Update branch $BRANCH"
 echo '1 - Android development'
+echo '2 - iOS development'
 echo ''
 echo ''
 read -n 1 -s -r -p 'Select build type, ESC to cancel...' key
@@ -88,9 +133,13 @@ UpdateRepo
 1)
 AndroidDevelopment
 ;;
+2)
+iOSDevelopment
+;;
 *)
 echo ''
 echo 'Unknown operation type! Logging out...'
 echo ''
 ;;
 esac
+ShowElapsedTime
