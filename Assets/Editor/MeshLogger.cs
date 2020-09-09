79feee89033b2b89f3b306825dc435daf6ca926a
Maksim <maksim.melnik.konstantinovich@gmail.com>
2020-09-09 15:40:58 +0300

    Model WIP

---
+++ Assets/Editor/MeshLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MeshLogger : MonoBehaviour
+{
+    private Mesh _mesh;
+    
+    public void LogInfo()
+    {
+        _mesh = GetComponent<MeshFilter>().sharedMesh;
+        
+    }
+}
+
+[CustomEditor(typeof(MeshLogger))]
+public class MeshLoggerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        MeshLogger meshLogger = target as MeshLogger;
+        
+        if (!meshLogger)
+            return;
+
+        if (GUILayout.Button("Update info"))
+        {
+            
+        }
+    }
+}
---
+++ Assets/InputController/TouchInput.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchInput : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private Vector2 startPoint;
+    private Vector2 currentPoint;
+    private Vector3 direction = Vector3.zero;
+    private IInput input;
+    private Transform _transform;
+
+    private void Start()
+    {
+        input = new MouseController();
+        _transform = transform;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        startPoint = input.GetPosition();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
+
+    private void Update()
+    {
+        currentPoint = input.GetPosition();
+        var normalizedDir = currentPoint - startPoint;
+        direction.Set(normalizedDir.x, 0f, normalizedDir.y);
+        MovePlayer(direction);
+    }
+
+    private void MovePlayer(Vector3 dir)
+    {
+        _transform.position = dir;
+    }
+}
+
+public class KeyboardController : IInput
+{
+    public Vector2 GetPosition()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class MouseController : IInput
+{
+    public Vector2 GetPosition()
+    {
+        return Input.mousePosition;
+    }
+}
+
+public class TouchController : IInput
+{
+    public Vector2 GetPosition()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+interface IInput
+{
+    Vector2 GetPosition();
+}
