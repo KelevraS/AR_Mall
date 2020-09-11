d31cf4dc22908168a2c5f56f9f3e4e0df418a629
Maksim <maksim.melnik.konstantinovich@gmail.com>
2020-09-11 13:54:29 +0300

    Added baked character animation

--- Assets/Scripts/ClothTimer.cs
+++ Assets/Scripts/ClothTimer.cs
@@ -7,15 +7,22 @@ public class ClothTimer : MonoBehaviour
 {
     private Material mat;
 
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private Animator anim;
 
     private void Start()
     {
         mat = GetComponent<MeshRenderer>().material;
+        anim.speed = speed;
     }
 
     private void Update()
     {
-        mat.SetFloat("_Timeposition", Mathf.PingPong(Time.time * speed, 1f));
+        float normalizedTime = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        float animationSpeedMultiplier = anim.GetCurrentAnimatorStateInfo(0).speed;
+        
+        mat.SetFloat("_Timeposition", animationSpeedMultiplier > 0 ? normalizedTime : 1  - normalizedTime);
+        
+        Debug.LogFormat("Normalized time: {0} \n Speed multiplier: {1}", normalizedTime, animationSpeedMultiplier);
     }
 }
